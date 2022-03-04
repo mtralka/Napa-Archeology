2f24ed1be1d4ea67eb96c7e804079d23208d9406
mtralka <mtralka@umd.edu>
2022-03-04 12:00:52 -0500

    remove pinea

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,5 @@
-import { createPinia } from "pinia";
 import "virtual:windi.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).use(createPinia()).mount("#app");
+createApp(App).mount("#app");
